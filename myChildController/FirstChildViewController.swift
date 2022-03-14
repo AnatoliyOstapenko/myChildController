7fb4d6e3b1c2cdae7463108b2a0baf35b1d60073
Anatolii Ostapenko <anatolii.ostapenko@avenga.com>
2022-03-14 20:06:04 +0200

    delete everything was done
    IOS Academy information is too complicated

--- myChildController/FirstChildViewController.swift
+++ myChildController/FirstChildViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FirstViewController.swift
 //  myChildController
 //
 //  Created by AnatoliiOstapenko on 14.03.2022.
@@ -7,16 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
-   
-    
+class FirstChildViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-    }
-    
-  
-}
 
+        view.backgroundColor = .systemRed
+    }
+
+}
