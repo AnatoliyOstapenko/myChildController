8a21f8f6a34d5b4bb2e5373b016834cd57b4f62b
Anatolii Ostapenko <anatolii.ostapenko@avenga.com>
2022-03-15 17:28:14 +0200

    add some constraints

--- myChildController/Models/PopUpModel.swift
+++ myChildController/Models/PopUpModel.swift
@@ -13,7 +13,8 @@ class PopUpModel: UIView {
     private let titleLable: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = .systemFont(ofSize: 20, weight: .thin)
+        label.font = .systemFont(ofSize: 28, weight: .bold)
+        label.textAlignment = .center
         label.text = "PopUp title"
         return label
     }()
@@ -22,7 +23,9 @@ class PopUpModel: UIView {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = .systemFont(ofSize: 20, weight: .thin)
-        label.text = "SubTitle"
+        label.textAlignment = .center
+        label.numberOfLines = 3
+        label.text = "first row of text \n second row of text \n third row of text"
         return label
     }()
     
@@ -39,6 +42,8 @@ class PopUpModel: UIView {
     fileprivate lazy var popUpStack: UIStackView = {
         let stack = UIStackView(arrangedSubviews: [titleLable, subTitleLabel])
         stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.backgroundColor = .systemCyan
+        stack.axis = .vertical // specify a stack as vertical
         return stack
     }()
 
@@ -54,10 +59,12 @@ class PopUpModel: UIView {
         popUpContainer.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.5).isActive = true
         
         popUpContainer.addSubview(popUpStack) // add UIStackView to my container view
-        popUpStack.topAnchor.constraint(equalTo: popUpContainer.topAnchor).isActive = true
+        //popUpStack.topAnchor.constraint(equalTo: popUpContainer.topAnchor).isActive = true
         popUpStack.leadingAnchor.constraint(equalTo: popUpContainer.leadingAnchor).isActive = true
         popUpStack.trailingAnchor.constraint(equalTo: popUpContainer.trailingAnchor).isActive = true
-        popUpStack.bottomAnchor.constraint(equalTo: popUpContainer.bottomAnchor).isActive = true
+        //popUpStack.bottomAnchor.constraint(equalTo: popUpContainer.bottomAnchor).isActive = true
+        popUpStack.heightAnchor.constraint(equalTo: popUpContainer.heightAnchor, multiplier: 0.5).isActive = true
+        popUpStack.centerYAnchor.constraint(equalTo: popUpContainer.centerYAnchor).isActive = true
         
     }
     
