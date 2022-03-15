220a16ec462b0f18d14b42e09abcef0fd1dead36
Anatolii Ostapenko <anatolii.ostapenko@avenga.com>
2022-03-15 16:28:46 +0200

    add labels to stack view in PopUp class

--- myChildController/Models/PopUpModel.swift
+++ myChildController/Models/PopUpModel.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class PopUpModel: UIView {
     
-    
     private let titleLable: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -19,7 +18,7 @@ class PopUpModel: UIView {
         return label
     }()
     
-    private let subTitle: UILabel = {
+    private let subTitleLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = .systemFont(ofSize: 20, weight: .thin)
@@ -27,34 +26,38 @@ class PopUpModel: UIView {
         return label
     }()
     
+    // Create container UIView
     private let popUpContainer: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .red
+        view.backgroundColor = .white
         view.layer.cornerRadius = 24
-        
         return view
     }()
     
-    private lazy var popUpStack: UIStackView = {
-        let stack = UIStackView(arrangedSubviews: [titleLable, subTitle])
-        popUpContainer.translatesAutoresizingMaskIntoConstraints = false
+    // Create UIStackView
+    fileprivate lazy var popUpStack: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [titleLable, subTitleLabel])
+        stack.translatesAutoresizingMaskIntoConstraints = false
         return stack
     }()
-    
-    
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = .systemGray
         self.frame = UIScreen.main.bounds
-        
         addSubview(popUpContainer) // always before constraints!
+        
         popUpContainer.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
         popUpContainer.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
         popUpContainer.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.7).isActive = true
         popUpContainer.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.5).isActive = true
         
+        popUpContainer.addSubview(popUpStack) // add UIStackView to my container view
+        popUpStack.topAnchor.constraint(equalTo: popUpContainer.topAnchor).isActive = true
+        popUpStack.leadingAnchor.constraint(equalTo: popUpContainer.leadingAnchor).isActive = true
+        popUpStack.trailingAnchor.constraint(equalTo: popUpContainer.trailingAnchor).isActive = true
+        popUpStack.bottomAnchor.constraint(equalTo: popUpContainer.bottomAnchor).isActive = true
         
     }
     
